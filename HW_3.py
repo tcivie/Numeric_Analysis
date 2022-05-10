4851d0c16e24e527b11e28fb0b4c7017059cc770
Gleb Tcivie <gleb@tcivie.com>
2022-05-10 17:17:39 +0300

    Added the Newton_Raphson method + Some typos in the file

--- HW_3.py
+++ HW_3.py
@@ -1,18 +1,20 @@
-def Bisection_Method(polynome, distance, epsilon=0.0001):
+MAX_RUNS = 100
+
+
+def Bisection_Method(polynomial, distance, epsilon=0.0001):
     """
-    Finds the root of the polynome using the bisection method
-    :param polynome: The polynome EX: lambda x: (x*x) - 4
-    :param distance: The distance to look in the solution for the polynome
+    Finds the root of the Polynomial using the bisection method
+    :param polynomial: The Polynomial EX: lambda x: (x*x) - 4
+    :param distance: The distance to look in the solution for the Polynomial
     :param epsilon: The max distance to stop
     :return: The solution to the problem with epsilon as the max error
     """
     mid = 0
-    MAX_RUNS = 100
     i = 0
     while abs(distance[0] - distance[1]) > epsilon and i < MAX_RUNS:
         # Run till the distance between the points is the lowest or passed max runs
         mid = (distance[0] + distance[1]) / 2
-        if polynome(distance[0]) * polynome(mid) > 0:
+        if polynomial(distance[0]) * polynomial(mid) > 0:
             distance[0] = mid
         else:
             distance[1] = mid
@@ -21,6 +23,17 @@ def Bisection_Method(polynome, distance, epsilon=0.0001):
     if i == MAX_RUNS:
         print("Error: Couldn't find the point due to max runs reached")
     return mid
+
+
+def Newton_Raphson(polynomial, distance, epsilon=0.0001):
+    """
+    The method solves the root of the Polynomial in the given range using the newton Raphson method
+    :param polynomial: The Polynomial to solve
+    :param distance: The range where to look (Array<float>[2])
+    :param epsilon: The error range
+    :return:
+    """
+    pass
 
 
 print(Bisection_Method(lambda x: (x * x) - 4, [-1, 3]))
